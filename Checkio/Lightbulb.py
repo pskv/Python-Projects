96b6afb88040bef41b63815fa62def45afc9e5ba
MI <peshikovs@gmail.com>
2022-02-22 12:09:17 +0300

    Completed task "Lightbulb Start Watching".

---
+++ Checkio/Lightbulb.py
@@ -0,0 +1,119 @@
+from datetime import datetime
+from typing import List, Optional
+
+
+def sum_light(els: List[datetime], start_watching: Optional[datetime] = None) -> int:
+    # return sum(map(lambda x: (x[1]-x[0]).total_seconds(), zip(els[0::2], els[1::2])))
+    total_sec = 0
+    for dates in list(zip(els[0::2], els[1::2])):
+        if start_watching is None or start_watching <= dates[0]:
+            total_sec += (dates[1] - dates[0]).total_seconds()
+        elif dates[0] < start_watching < dates[1]:
+            total_sec += (dates[1] - start_watching).total_seconds()
+    return total_sec
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ]
+            # datetime(2015, 1, 12, 10, 0, 5),
+        )
+    )
+
+    assert (
+        sum_light(
+            els=[
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            start_watching=datetime(2015, 1, 12, 10, 0, 5),
+        )
+        == 5
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            datetime(2015, 1, 12, 10, 0, 0),
+        )
+        == 10
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 11, 0, 0),
+        )
+        == 610
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 11, 0, 10),
+        )
+        == 600
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 10, 10, 0),
+        )
+        == 620
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+                datetime(2015, 1, 12, 11, 10, 11),
+                datetime(2015, 1, 12, 12, 10, 11),
+            ],
+            datetime(2015, 1, 12, 12, 10, 11),
+        )
+        == 0
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+                datetime(2015, 1, 12, 11, 10, 11),
+                datetime(2015, 1, 12, 12, 10, 11),
+            ],
+            datetime(2015, 1, 12, 12, 9, 11),
+        )
+        == 60
+    )
+
+    print("The second mission in series is done? Click 'Check' to earn cool rewards!")
