6bf37af95466bd387b8ec73a7713ef76cac6e04c
MI <peshikovs@gmail.com>
2022-03-11 12:38:23 +0300

    First experience with PyPlot

--- XML/OSM_to_XML_2.py
+++ XML/OSM_to_XML_2.py
@@ -6,12 +6,20 @@ fin.close()
 
 parsedxml = xmltodict.parse(xml)
 total_cnt = 0
-for el in parsedxml['osm']['node']:
+for el in (parsedxml['osm']['node'] + parsedxml['osm']['way']):
+    # if el.get('@id') != "416170216":
+    #     continue
     if el.get('tag') is not None:
         d = el.get('tag')
         if isinstance(d, list):
-            if d[0].get('@k') == 'amenity' and d[0].get('@v') == 'fuel':
+            for el2 in d:
+                if el2.get('@k') == 'amenity' and el2.get('@v') == 'fuel':
+                    total_cnt += 1
+            # if d[0].get('@k') == 'amenity' and d[0].get('@v') == 'fuel':
+            #     total_cnt += 1
+            #     print(d)
+        elif isinstance(d, dict):
+            if d.get('@k') == 'amenity' and d.get('@v') == 'fuel':
                 total_cnt += 1
-        elif d.get('@k') == 'amenity' and d.get('@v') == 'fuel':
-            total_cnt += 1
+                # print(d)
 print(total_cnt)
