58df1f2bc66e56e95fdddf6527d6c3a0c104e5e2
MI <peshikovs@gmail.com>
2022-03-21 17:27:54 +0300

    Enigma. V0.2

--- Small_tasks_for_practice/Enigma.py
+++ Small_tasks_for_practice/Enigma.py
@@ -33,15 +33,21 @@ def reflector(symbol, n):
         if symbol in seq:
             return seq[(seq.index(symbol)+1) % 2]
 
+def shift(symbol, n):
+    return chr(((ord(symbol)+n-65) % 26)+65)
 
-def enigma(text, ref, rot1, rot2, rot3):
-    # res = ''
-    # for symb in text:
-    #     res += rotor(rotor(rotor(reflector(rotor(rotor(rotor(symb, rot3), rot2), rot1), ref), rot1, True), rot2, True), rot3, True)
-    # return res
-    return ''.join([rotor(rotor(rotor(reflector(rotor(rotor(rotor(symb, rot3), rot2), rot1), ref), rot1, True), rot2, True), rot3, True) for symb in text.upper() if symb.isalpha()])
+# def enigma(text, ref, rot1, rot2, rot3):
+#     return ''.join([rotor(rotor(rotor(reflector(rotor(rotor(rotor(symb, rot3), rot2), rot1), ref), rot1, True), rot2, True), rot3, True) for symb in text.upper() if symb.isalpha()])
+
+def enigma(text, ref, rot1, shift1, rot2, shift2, rot3, shift3):
+    return ''.join([shift(rotor(shift(rotor(shift
+            (rotor(shift(reflector(shift(rotor(shift(rotor(shift(rotor(shift(symb, shift3), rot3
+            ),shift2-shift3), rot2), shift1-shift2), rot1), -shift1), ref), shift1), rot1, True
+             ), shift2 - shift1), rot2, True), shift3-shift2), rot3, True), -shift3)
+            for symb in text.upper() if symb.isalpha()])
+
 
 # print(rotor('S', 3))
 # print(reflector('Y', 1))
 # print(enigma('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 1, 1, 2, 3))
-print(enigma('Some encripted text', 1, 1, 2, 3))
+print(enigma('Some encripted text', 1, 1, -1, 2, 2, 3, -1))
