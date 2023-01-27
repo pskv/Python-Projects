a263d9be49992ae5054cfb7c200eb0ec4bb45bb9
MI <peshikovs@gmail.com>
2023-01-27 14:44:57 +0300

    Completed "Searchlights" task

--- Checkio/Searchlights.py
+++ Checkio/Searchlights.py
@@ -10,20 +10,21 @@ def searchlights(polygons, lights):
 
         r = round(polygon[2]/2/sin(pi/polygon[3]), 4)  # radius
         p = Shape(x=polygon[0], y=polygon[1]-r, r=r, p_cnt=polygon[3])
-        print(p)
 
-        point_angles = set([i * (360 / p.p_cnt) for i in range(p.p_cnt)])
+        point_angles = set([i * (2 * pi / p.p_cnt) for i in range(p.p_cnt)])
 
         if p.x - p.r < 0:  # check points with negative X coordinates
-            angle = acos(p.x / p.r) * 180 / pi
+            angle = acos(p.x / p.r)
+            angle = (angle - (pi/2), 2*pi-angle-(pi/2))
             point_angles = set(
-                filter(lambda x: angle - 90 <= x <= 360 - angle - 90 or angle - 90 <= x - 360 <= 360 - angle - 90,
+                filter(lambda x: angle[0] <= x <= angle[1] or angle[0] <= x - 2*pi <= angle[1],
                        point_angles))
 
         if p.y - p.r < 0:  # check points with negative Y coordinates
-            angle = acos(p.y / p.r) * 180 / pi
+            angle = acos(p.y / p.r)
+            angle = (angle - pi, pi-angle)
             point_angles = set(
-                filter(lambda x: angle - 180 <= x <= 180 - angle or angle - 180 <= x - 360 <= 180 - angle,
+                filter(lambda x: angle[0] <= x <= angle[1] or angle[0] <= x - 2*pi <= angle[1],
                        point_angles))
 
         visible_points = set()
@@ -34,38 +35,35 @@ def searchlights(polygons, lights):
 
             if d == 0:  # Centers are equal
                 if p.r <= l.r:
-                    # res += len(point_angles)
                     visible_points |= point_angles
                 continue
             if d == p.r+l.r:  # One point intersection
-                # res += 1
-                if l.x>p.x:
-                    visible_points |= acos((l.y - p.y) / d) * 180 / pi
+                if l.x > p.x:
+                    visible_points |= acos((l.y - p.y) / d)
                     continue
-                visible_points |= 360 - acos((l.y - p.y) / d) * 180 / pi
+                visible_points |= 2*pi - acos((l.y - p.y) / d)
                 continue
             if d > p.r+l.r:  # No intersection
                 continue
-            if p.r > d+l.r:  # Circle is in the polygon
+            if p.r > d+l.r:  # Circle is in the polygon. No intersection
                 continue
             if l.r >= d + p.r:  # Polygon is in the circle
-                # res += len(point_angles)
                 visible_points |= point_angles
                 continue
 
             #  Two point intersection
-            angle = (acos((d**2+p.r**2-l.r**2)/(2*d*p.r)) * 180 / pi)  # angle
+            angle = acos((d**2+p.r**2-l.r**2)/(2*d*p.r))
             if l.x >= p.x:
-                angle2 = acos((l.y-p.y)/d) * 180 / pi
+                angle2 = acos((l.y-p.y)/d)
             else:
-                angle2 = 360-acos((l.y-p.y)/d) * 180 / pi
+                angle2 = 2*pi - acos((l.y-p.y)/d)
 
 
             visible_points |= set(
                 filter(
                     lambda x: angle2-angle <= x <= angle2+angle or
-                              angle2-angle <= x - 360 <= angle2+angle or
-                              angle2-angle <= x + 360 <= angle2+angle
+                              angle2-angle <= x - 2*pi <= angle2+angle or
+                              angle2-angle <= x + 2*pi <= angle2+angle
                     ,point_angles))
             continue
         res += len(visible_points)
@@ -83,9 +81,9 @@ if __name__ == "__main__":
     print(searchlights([[2,2,2,7],[1,5,2,8]],[[1,2,2],[3,5,1],[2,3,2],[8,1,1],[4,3,2],[4,4,1]]))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    # assert (searchlights([(2, 3, 2, 3)], [(1, 2, 1)])) == 1, "regular triangle"
-    # assert (searchlights([(4, 5, 2, 4)], [(4, 4, 3)])) == 4, "square"
-    # assert (searchlights([(6, 7, 2, 5)], [(2, 3, 2)])) == 0, "regular pentagon"
-    # assert (searchlights([(4, 2, 2, 6)], [(4, 2, 3)])) == 3, "regular hexagon"
-    # assert (searchlights([(1, 7, 2, 8)], [(0, 5, 4)])) == 5, "regular octagon"
+    assert (searchlights([(2, 3, 2, 3)], [(1, 2, 1)])) == 1, "regular triangle"
+    assert (searchlights([(4, 5, 2, 4)], [(4, 4, 3)])) == 4, "square"
+    assert (searchlights([(6, 7, 2, 5)], [(2, 3, 2)])) == 0, "regular pentagon"
+    assert (searchlights([(4, 2, 2, 6)], [(4, 2, 3)])) == 3, "regular hexagon"
+    assert (searchlights([(1, 7, 2, 8)], [(0, 5, 4)])) == 5, "regular octagon"
     print("Coding complete? Click 'Check' to earn cool rewards!")
